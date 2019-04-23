a1e75c28b135b2e55e41307319a0c23d12c660b8
Jess Rezac <jess.rezac@gmail.com>
2019-04-23 22:54:06 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,8 @@
 def oxford_comma(array)
-  if array.size > 1
-    last_value = array.last
-    array.pop
-    list = array.join(" , ")
-    list << "and #{last_value}"
-    puts list
+  if array.size >= 2
+    puts "i haven't done this yet"
+  elsif array.size == 1
+    array.join(" and ")
   else
     array.join
   end
