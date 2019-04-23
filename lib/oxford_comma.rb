d98f15e732e9e871e97aaa7638d6c49102e279e3
Jess Rezac <jess.rezac@gmail.com>
2019-04-23 22:56:31 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     last_item = array.last
     array.pop
     new_string = array.join(", ")
-    new_string << "and #{last_item}"
+    new_string << ", and #{last_item}"
   elsif array.size == 2
     array.join(" and ")
   else
